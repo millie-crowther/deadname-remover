e715174c924cb474a7d1ca9a74ba84dd38f95460
millie <mbc15@ic.ac.uk>
2017-03-31 13:06:23 +0100

    script now works with an arbitrary user's account

--- twitter_remover.py
+++ twitter_remover.py
@@ -1,31 +1,45 @@
 import tweepy
+import os
+
+# access tokens for @HackathonAcct
+#access_token = "847744745090686976-vTf0XUilEVh3ashprVGEc3Rc8ohdZco"
+#access_token_secret = "DgbIYYQyuGQR40pjfb0ZWKiPrIdVNlQuHNAvFqCARnxO2"
 
 #identify ourselves as registered app
 auth = tweepy.OAuthHandler("kGx0VVO7HR9rTrKCGOne7DJDn", "bniWiIqupj9OzQRoKAl6xtcAeAtrNbW6wesKNvD9iXX25xjDUS")
-auth.set_access_token("847744745090686976-vTf0XUilEVh3ashprVGEc3Rc8ohdZco", "DgbIYYQyuGQR40pjfb0ZWKiPrIdVNlQuHNAvFqCARnxO2")
+
+# authorise the users account
+redirect_url = auth.get_authorization_url()
+os.system("firefox " + redirect_url)
+pin = raw_input("Please input the PIN given to you in your broswer\n>")
+token = auth.get_access_token(verifier=pin)
+access_token = token[0]
+access_token_secret = token[1]
+
+#auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-#delete tweets containg deadname
-first_name = raw_input("Please enter your old first name\n>")
-last_name = raw_input("Please enter your old last name\n>")
-full_name = first_name + " " + last_name
-handle = raw_input("Please enter your old twitter handle\n>")
-
 #delete a users own tweets
+first_name = raw_input("Please enter your old first name\n>").upper()
+last_name = raw_input("Please enter your old last name\n>").upper()
+handle = raw_input("Please enter your old twitter handle\n>").upper()
+
+full_name = first_name + " " + last_name
 print("Searching your tweets...")
 public_tweets = tweepy.Cursor(api.user_timeline).items()
+
 for tweet in public_tweets:
-    if full_name in tweet.text:
-        print("Deleting this tweet becasuse it contained your old name: ")
+    if full_name in tweet.text.upper():
+        print("Deleting this tweet because it contained your old name: ")
         print(tweet.text)
         api.destroy_status(tweet.id)
 
-    elif handle in tweet.text:
+    elif handle in tweet.text.upper():
         print("Deleting this tweet becasuse it contained your old handle: ")
         print(tweet.text)
         api.destroy_status(tweet.id)
 
-    elif first_name in tweet.text:
+    elif first_name in tweet.text.upper():
         print("Old first name found. Do you want to delete this tweet? [y/n]")
         print(tweet.text)
         response = raw_input(">")
@@ -39,7 +53,4 @@ for tweet in public_tweets:
         print("Ignoring this tweet: " + tweet.text)
 
 #ask other users to delete tweets
-print("Searching other people's tweets...")
-query = api.search(q=handle)
-for tweet in query:
-    print(tweet.text) 
+print("Searching your followers' tweets...")
